f7d5dda3148b7c53595440d8cbc7bf8a0d99dd9d
Michael Alaniz <michaelalaniz76@gmail.com>
2024-08-27 16:58:11 -0700

    updates to the controller

--- backend/src/main/java/games/net/Project/Artifact/Users/UserService.java
+++ backend/src/main/java/games/net/Project/Artifact/Users/UserService.java
@@ -1,5 +1,20 @@
 package games.net.Project.Artifact.Users;
 
+import org.apache.catalina.User;
+import org.springframework.stereotype.Service;
+import java.util.*;
+
+@Service
 public class UserService {
     
+    public List<Users> getUsers(){
+        return List.of(
+            new Users(
+            1L,
+            "michaelalaniz76@gmail.com",
+            "peaches2001$",
+            "mialaniz")
+        );
+
+    }
 }
--- backend/src/main/java/games/net/Project/Artifact/Users/UsersController.java
+++ backend/src/main/java/games/net/Project/Artifact/Users/UsersController.java
@@ -1,5 +1,8 @@
 package games.net.Project.Artifact.Users;
 
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "api/users")
 public class UsersController {
     
-    
+    private final UserService userService;
 
+    @Autowired
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<Users> getUsers() {
+        return userService.getUsers();
+    }
 }
